06fc63241eb9cbcd4d1c8a07684fc9bb42fe14a7
Patrick Richardson <setfloat@gmail.com>
2016-11-19 13:27:26 -0800

    Solved multiple aria parts of course

--- lesson5-semantics-aria/13-combobox/combobox.js
+++ lesson5-semantics-aria/13-combobox/combobox.js
@@ -170,6 +170,11 @@
                 this.hide();
             } else {
                 // FIXME: ChromeVox reports the wrong list size and position
+                for (let i = 0; i < visibleItems.length; i++) {
+                  let item = visibleItems[i];
+                  item.setAttribute('aria-posinset', i + 1);
+                  item.setAttribute('aria-setsize', this.visibleItems.length);
+                }
             }
         },
 
@@ -237,6 +242,7 @@
             newActive.classList.add('active');
 
             // FIXME: need to ensure focus stays on textbox, but report active list option
+            this.textbox.setActiveDescendant(newActive);
         }
     };
 
